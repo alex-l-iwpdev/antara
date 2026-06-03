262a9949a755750020e74a4f8be28e1d1de125ae
lovik <admin@i-wp-dev.com>
2026-06-03 14:01:13 +0200

    fix GPU iPhone

--- src/php/Main.php
+++ src/php/Main.php
@@ -20,7 +20,7 @@ class Main {
 	/**
 	 * Theme version.
 	 */
-	const THEME_VERSION = '1.2.11';
+	const THEME_VERSION = '1.2.13';
 
 	/**
 	 * Internal flag to avoid infinite loops while syncing WPML statuses.
