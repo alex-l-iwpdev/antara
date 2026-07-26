91144437affcd3223e8db62bd016ce5528e1654c
lovik <admin@i-wp-dev.com>
2026-07-26 10:33:57 +0200

    up version fix css

--- src/php/Main.php
+++ src/php/Main.php
@@ -20,7 +20,7 @@ class Main {
 	/**
 	 * Theme version.
 	 */
-	const THEME_VERSION = '1.2.23';
+	const THEME_VERSION = '1.2.25';
 
 	/**
 	 * Internal flag to avoid infinite loops while syncing WPML statuses.
