3846ed80f882eaaa05ddfcd00af10406440de2a8
lovik <admin@i-wp-dev.com>
2026-08-09 11:10:41 +0200

    update css/js

--- src/js/layout/Forms.js
+++ src/js/layout/Forms.js
@@ -1,12 +1,20 @@
 const Forms = ( $ ) => {
+	$('.footer-email').click(function(){
+		$('.gfield--type-name').removeClass('name-hidden'); 
+	});
 	// Предотвращаем повторную инициализацию
 	const body = document.body;
 	if ( body && body.dataset.formsInitialized === 'true' ) return;
 	if ( body ) body.dataset.formsInitialized = 'true';
 
 	const cleanups = [];
-
-	document.querySelectorAll( '.form-group' ).forEach( ( group ) => {
+	if($('select').length){
+		$('select').selectric({
+			maxHeight: 368,
+		}); 
+	}
+	document.querySelectorAll( '.form-group' ).forEach( ( group ) => { 
+		console.log(group);
 		const input = group.querySelector( 'input' );
 		const label = group.querySelector( 'label' );
 		if ( ! input || ! label ) return;
@@ -39,9 +47,26 @@ const Forms = ( $ ) => {
 			}
 		} );
 	}, { once: true } );
-	$('.footer-email').focus(function(){
-		$('.gfield--type-name').removeClass('name-hidden');
-	});
+	const input = $('[class*="gfield--input-"]:not(.select) input, [class^="gfield--input-"]:not(.select) input, .textarea textarea') 
+	if ( input.length ) {
+		input.focus( function() {
+			$( this ).parents('[class*="gfield--input-"]:not(.select), .textarea').addClass( 'focus' );
+		} );
+		input.blur( function() {
+			if ( $( this ).val().length === 0 ) {
+				$( this ).parents('[class*="gfield--input-"]:not(.select), .textarea').removeClass( 'focus' );
+			}
+		} );
+		input.each( function() {
+			if ( $( this ).val().length === 0 ) {
+				$( this ).parents('[class*="gfield--input-"]:not(.select)w, .textarea').removeClass( 'focus' );
+			} else {
+				$( this ).parents('[class*="gfield--input-"]:not(.select), .textarea').addClass( 'focus' );
+			}
+		} );
+	}
+
+	
 };
 
 export default Forms;
